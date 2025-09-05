c77762417127308bc08d5fe3de933179f02348b8
Sumanshu Nankana <sumanshunankana@gmail.com>
2025-09-05 22:39:24 +0100

    added solution for 1757 in pandas

---
+++ leetcode/1757_recyclable_and_low_fat_products.py
@@ -0,0 +1,32 @@
+"""
+1757 Recyclable and Low Fat Products
+https://leetcode.com/problems/recyclable-and-low-fat-products/description/
+"""
+
+import pandas as pd
+
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    # Whenever we have two conditions, we will use bracket
+    condition = (products["low_fats"] == "Y") & (products["recyclable"] == "Y")
+
+    # Note: products[condition]['product_id'] returns a Series
+    # To return a DataFrame, put column names in a list
+    return products[condition][["product_id"]]
+
+
+# Example usage/test
+if __name__ == "__main__":
+    data = [
+        ["0", "Y", "N"],
+        ["1", "Y", "Y"],
+        ["2", "N", "Y"],
+        ["3", "Y", "Y"],
+        ["4", "N", "N"],
+    ]
+    products = pd.DataFrame(
+        data, columns=["product_id", "low_fats", "recyclable"]
+    ).astype({"product_id": "int64", "low_fats": "category", "recyclable": "category"})
+    output = find_products(products)
+    print(output)
+    print(type(output))
