83f7955586a72d9f8a2287596d9a7da522132a7a
Sumanshu Nankana <sumanshunankana@gmail.com>
2025-09-06 22:25:41 +0100

    added solution for 1148

--- leetcode/1148_article_views_I.py
+++ leetcode/1148_article_views_I.py
@@ -7,7 +7,13 @@ import pandas as pd
 
 
 def article_views(views: pd.DataFrame) -> pd.DataFrame:
-    pass
+    condition = views["author_id"] == views["viewer_id"]
+    return (
+        views[condition][["author_id"]]
+        .drop_duplicates()
+        .sort_values(by="author_id")
+        .rename(columns={"author_id": "id"})
+    )
 
 
 if __name__ == "__main__":
