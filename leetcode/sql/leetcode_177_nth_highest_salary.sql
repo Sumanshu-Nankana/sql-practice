8219b34f023438d7458a46718bd5e43ac054471e
Sumanshu Nankana <sumanshunankana@gmail.com>
2025-10-11 17:14:01 +0100

    added solution for leetcode 177

---
+++ leetcode/sql/leetcode_177_nth_highest_salary.sql
@@ -0,0 +1,17 @@
+-- We need to give e.salary, because SALARY is a output variable as well in Function statement
+-- if we normally use SELECT salary, DENSE_RANK....
+-- Then query will give error as - column reference "salary" is ambiguous
+-- Because query get confuse which 'salary' we are referring
+-- PostgreSQL can’t tell if salary refers to: the table column employee.salary, or
+-- the output variable salary from your function’s RETURNS TABLE (salary INT) clause.
+
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
+BEGIN
+  RETURN QUERY (
+    -- Write your PostgreSQL query statement below.
+    with cte as (SELECT e.salary, DENSE_RANK() OVER (ORDER BY e.SALARY DESC) AS RANKING FROM EMPLOYEE e)
+    SELECT DISTINCT COALESCE(e.SALARY, NULL) FROM CTE e WHERE RANKING = N
+
+  );
+END;
+$$ LANGUAGE plpgsql;
