e3473713bdba2e7f852b03b668a4d65a916a3709
Sumanshu Nankana <sumanshunankana@gmail.com>
2025-10-19 20:33:03 +0100

    added solution for leetcode 182

---
+++ leetcode/sql/leetcode_182_duplicate_emails.sql
@@ -0,0 +1,11 @@
+-- Create Table
+Create table If Not Exists Person (id int, email varchar(255))
+
+-- Insert Data
+Truncate table Person
+insert into Person (id, email) values ('1', 'a@b.com')
+insert into Person (id, email) values ('2', 'c@d.com')
+insert into Person (id, email) values ('3', 'a@b.com')
+
+-- Write your PostgreSQL query statement below
+SELECT email FROM PERSON group by email having count(*) > 1
