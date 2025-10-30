845d1d9c52fd7309e87e34d26ded35b8c41a1408
Sumanshu Nankana <sumanshunankana@gmail.com>
2025-10-30 18:01:41 +0000

    added solution for 511

---
+++ leetcode/sql/leetcode_511_game_play_analysis_I.sql
@@ -0,0 +1,20 @@
+-- Create Table
+Create table If Not Exists Activity (player_id int, device_id int, event_date date, games_played int)
+
+-- Insert Data
+Truncate table Activity
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-03-01', '5')
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-05-02', '6')
+insert into Activity (player_id, device_id, event_date, games_played) values ('2', '3', '2017-06-25', '1')
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '1', '2016-03-02', '0')
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '4', '2018-07-03', '5')
+
+-- Write your PostgreSQL query statement below
+-- Method1
+with cte as(
+SELECT *, DENSE_RANK() OVER(PARTITION BY player_id order by event_date) as rnk from activity
+)
+select player_id, event_date as first_login from cte where rnk = 1
+
+-- Method2
+select player_id, min(event_date) as first_login from activity group by player_id
