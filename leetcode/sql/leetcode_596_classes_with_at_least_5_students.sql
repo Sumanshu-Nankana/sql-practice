44bc632c9152258c0d6a870c8a6ae78729b3996e
Sumanshu Nankana <sumanshunankana@gmail.com>
2025-10-31 18:38:22 +0000

    added leetcode solution 596

---
+++ leetcode/sql/leetcode_596_classes_with_at_least_5_students.sql
@@ -0,0 +1,17 @@
+-- Create Table
+Create table If Not Exists Courses (student varchar(255), class varchar(255));
+
+-- Insert Data
+Truncate table Courses;
+insert into Courses (student, class) values ('A', 'Math');
+insert into Courses (student, class) values ('B', 'English');
+insert into Courses (student, class) values ('C', 'Math');
+insert into Courses (student, class) values ('D', 'Biology');
+insert into Courses (student, class) values ('E', 'Math');
+insert into Courses (student, class) values ('F', 'Computer');
+insert into Courses (student, class) values ('G', 'Math');
+insert into Courses (student, class) values ('H', 'Math');
+insert into Courses (student, class) values ('I', 'Math');
+
+-- Write your PostgreSQL query statement below
+SELECT class from courses group by class having count(*) >= 5
