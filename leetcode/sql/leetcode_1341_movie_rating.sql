9e59eaf9d13321819b0efd0d8f9d8422739610c0
Sumanshu Nankana <sumanshunankana@gmail.com>
2025-11-02 17:43:48 +0000

    added leetcode 1341 solution

---
+++ leetcode/sql/leetcode_1341_movie_rating.sql
@@ -0,0 +1,38 @@
+-- Create Table
+Create table If Not Exists Movies (movie_id int, title varchar(30));
+Create table If Not Exists Users (user_id int, name varchar(30));
+Create table If Not Exists MovieRating (movie_id int, user_id int, rating int, created_at date);
+
+-- Insert Data
+Truncate table Movies;
+insert into Movies (movie_id, title) values ('1', 'Avengers');
+insert into Movies (movie_id, title) values ('2', 'Frozen 2');
+insert into Movies (movie_id, title) values ('3', 'Joker');
+Truncate table Users;
+insert into Users (user_id, name) values ('1', 'Daniel');
+insert into Users (user_id, name) values ('2', 'Monica');
+insert into Users (user_id, name) values ('3', 'Maria');
+insert into Users (user_id, name) values ('4', 'James');
+Truncate table MovieRating;
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('1', '1', '3', '2020-01-12');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('1', '2', '4', '2020-02-11');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('1', '3', '2', '2020-02-12');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('1', '4', '1', '2020-01-01');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('2', '1', '5', '2020-02-17');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('2', '2', '2', '2020-02-01');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('2', '3', '2', '2020-03-01');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('3', '1', '3', '2020-02-22');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('3', '2', '4', '2020-02-25');
+
+-- Write your PostgreSQL query statement below
+with cte as(
+select m.*, u.name as user_name, movies.title from movierating m left join users u
+on m.user_id = u.user_id
+left join movies on movies.movie_id = m.movie_id
+)
+(select title as results from cte
+where created_at >= '2020-02-01' and created_at < '2020-03-01' group by title
+order by avg(rating) desc, title asc limit 1)
+UNION ALL
+(select user_name as results from cte group by user_name order by count(*) desc,
+user_name asc limit 1)
