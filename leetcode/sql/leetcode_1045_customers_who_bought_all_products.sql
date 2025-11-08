7ba64950605e9d83764588aa2b282f3b016f2908
Sumanshu Nankana <sumanshunankana@gmail.com>
2025-11-08 15:36:36 +0000

    added leetcode 1045 solution

--- leetcode/sql/leetcode_1045_customers_who_bought_all_products.sql
+++ leetcode/sql/leetcode_1045_customers_who_bought_all_products.sql
@@ -16,3 +16,12 @@ insert into Product (product_key) values ('6');
 -- Write your PostgreSQL query statement below
 select customer_id from customer group by customer_id
 having count(DISTINCT product_key) = (SELECT COUNT(product_key) from product)
+
+-- Method2
+-- If we want to match by Values (Not by Count)
+with cte as(
+SELECT customer_id, STRING_AGG(DISTINCT CAST(product_key AS TEXT), ',' ORDER BY CAST(product_key AS TEXT)) as customer_products
+FROM customer group by customer_id),
+cte1 as(
+select STRING_AGG(CAST(product_key as text), ',' ORDER BY cast(product_key as text)) as all_products from product)
+select customer_id from cte join cte1 on cte.customer_products = cte1.all_products
