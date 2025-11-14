b01942006e2a3dc86e22a40d305e9e0016aaf6e6
Sumanshu Nankana <sumanshunankana@gmail.com>
2025-11-14 22:59:07 +0000

    added solution for leetcode 1251

---
+++ leetcode/sql/leetcode_1251_average_selling_price.sql
@@ -0,0 +1,26 @@
+-- Create Table
+Create table If Not Exists Prices (product_id int, start_date date, end_date date, price int);
+Create table If Not Exists UnitsSold (product_id int, purchase_date date, units int);
+
+-- Insert Data
+Truncate table Prices;
+insert into Prices (product_id, start_date, end_date, price) values ('1', '2019-02-17', '2019-02-28', '5');
+insert into Prices (product_id, start_date, end_date, price) values ('1', '2019-03-01', '2019-03-22', '20');
+insert into Prices (product_id, start_date, end_date, price) values ('2', '2019-02-01', '2019-02-20', '15');
+insert into Prices (product_id, start_date, end_date, price) values ('2', '2019-02-21', '2019-03-31', '30');
+Truncate table UnitsSold;
+insert into UnitsSold (product_id, purchase_date, units) values ('1', '2019-02-25', '100');
+insert into UnitsSold (product_id, purchase_date, units) values ('1', '2019-03-01', '15');
+insert into UnitsSold (product_id, purchase_date, units) values ('2', '2019-02-10', '200');
+insert into UnitsSold (product_id, purchase_date, units) values ('2', '2019-03-22', '30');
+
+-- Write your PostgreSQL query statement below
+-- Write your PostgreSQL query statement below
+WITH CTE AS(
+SELECT p.product_id, ROUND(sum((p.price * u.units))/CAST(sum(u.units) AS DECIMAL), 2) as average_price FROM PRICES p LEFT JOIN UNITSSOLD u
+ON p.product_id = u.product_id and
+u.purchase_date >= p.start_date and
+u.purchase_date <= p.end_date
+GROUP BY p.product_id
+)
+select product_id,  COALESCE(average_price, 0) as average_price from cte
